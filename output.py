87f1d71f2e0d00ce914c3f965d44d16bb7ecca4e
Daniel Waterworth <da.waterworth@gmail.com>
2018-01-03 08:05:42 +0000

    Moved entry point

--- output.py
+++ output.py
@@ -1,11 +1,4 @@
-import sys
-
-import cps_conversion
-import importer
-import lexer
 import new_code_generator
-import plst_parser
-import type_checker
 
 class LLVMWriter:
     def __init__(self, fd):
@@ -69,7 +62,7 @@ class LLVMWriter:
             raise NotImplementedError()
 
     def writeout_declare(self, decl):
-        fd.write('declare ')
+        self.write('declare ')
         self.writeout_type(decl.return_type)
         self.write(' ')
         self.write(decl.name)
@@ -348,45 +341,3 @@ class LLVMWriter:
     def writeout_decls(self, decls):
         for decl in decls:
             self.writeout_decl(decl)
-
-def parse(src):
-    tokens = lexer.Lexer(src).lex()
-    return plst_parser.Parser(tokens).parse_file()
-
-checked_module_interfaces = {}
-checked_decls = {}
-modules = importer.Importer(parse).load_all(sys.argv[1])
-
-global_env = \
-    type_checker.Environment(
-        type_checker.global_type_environment,
-        type_checker.global_term_environment,
-    )
-
-for module_name, decls in modules:
-    env = \
-        type_checker.Environment(
-            {},
-            {},
-            global_env,
-            module_name = module_name,
-            modules = checked_module_interfaces,
-        )
-    decls = env.check_top_level_decls(decls)
-    checked_module_interfaces[module_name] = \
-        type_checker.ModuleInterface(
-            env.term_bindings,
-            env.type_bindings,
-        )
-    checked_decls[module_name] = decls
-
-cg = new_code_generator.CodeGenerator()
-
-for module_name, _ in modules:
-    type_checked_decls = checked_decls[module_name]
-    cg.generate(module_name, type_checked_decls)
-
-with open(sys.argv[2], 'w') as fd:
-    writer = LLVMWriter(fd)
-    writer.writeout_prelude()
-    writer.writeout_decls(cg.get_decls())
